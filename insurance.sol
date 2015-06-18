c05e5ccfa88f6a38c4a3dc711707ffc5e19b46ff
Mike Goldin <skmgoldin@gmail.com>
2015-06-18 10:47:35 -0400

    New constructor and contract constants.

--- insurance.sol
+++ insurance.sol
@@ -1,17 +1,24 @@
 contract Insurance {
   
+  mapping(address => member) members;
+  uint pool;        // Pool inputs minus pool outputs (how much cash is available).
+  uint fee;         // The fee to be paid every feeInterval to maintain membership.
+  uint feeInterval; // The frequency, in days, in which fees must be paid.
+  uint waitPeriod;  /* The period, in days, for which new members must maintain
+                       their membership before being able to make claims. */
+
   struct member {
     address addr;
     uint timeIn;
     uint totalInput;
-    uint insureAgainst;
+    uint totalBenefit;
   }
 
-  mapping(address => member) members;
-  uint pool;
-
-  function Insurance() {
+  function Insurance(uint fee, uint feeInterval, uint waitPeriod) {
     pool = 0;
+    this.fee = fee;
+    this.feeInterval = feeInterval;
+    this.waitPeriod = waitPeriod;
   }
 
   function joinPool(uint amount, uint insureAgainst) {
